7d6ddacaf823488e8e843c786b77cce26cc3ef00
ByteAtATime <ByteAtATime@proton.me>
2025-09-26 10:00:59 -0700

    feat: disable default micromark emphasis and strong behavior

--- src/micromark.ts
+++ src/micromark.ts
@@ -3,4 +3,5 @@ import { slackBoldConstruct } from "./micromark/bold";
 
 export const slackTokens = {
   text: { 42: slackBoldConstruct },
+  disable: { null: ["emphasis", "strong"] },
 } satisfies Extension;
