49b18c6981417900f7152831117c345151603f08
ByteAtATime <byteatatime@proton.me>
2025-09-25 18:55:38 -0700

    feat: add TypeScript definitions for Slack bold formatting

---
+++ src/global.d.ts
@@ -0,0 +1,9 @@
+export * from "micromark-util-types";
+
+declare module "micromark-util-types" {
+  interface TokenTypeMap {
+    slackBold: "slackBold";
+    slackBoldMarker: "slackBoldMarker";
+    slackBoldText: "slackBoldText";
+  }
+}
